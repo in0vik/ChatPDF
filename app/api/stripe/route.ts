9e2b0c62a9a6becba99f03b31b327d9fdce68052
Ilya Novik <ilya.novikk@gmail.com>
2024-09-22 14:09:29 +0200

    update pricing

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -7,24 +7,27 @@ import { auth, currentUser } from '@clerk/nextjs/server';
 import { eq } from 'drizzle-orm';
 import { NextResponse } from 'next/server';
 
-const return_url = process.env.NEXT_BASE_URL + '/'
+const return_url = process.env.NEXT_BASE_URL + '/';
 
 export async function GET() {
   try {
     const { userId } = await auth();
     const user = await currentUser();
-  
+
     if (!userId) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
 
-    const _userSubscriptions = await db.select().from(userSubscriptions).where(eq(userSubscriptions.userId, userId));
+    const _userSubscriptions = await db
+      .select()
+      .from(userSubscriptions)
+      .where(eq(userSubscriptions.userId, userId));
     if (_userSubscriptions[0] && _userSubscriptions?.[0]?.stripeCustomerId) {
       const stripeSession = await stripe.billingPortal.sessions.create({
         customer: _userSubscriptions[0].stripeCustomerId,
         return_url,
-      })
-      return NextResponse.json({ url: stripeSession.url })
+      });
+      return NextResponse.json({ url: stripeSession.url });
     }
 
     // user's first time trying to subscribe
@@ -41,25 +44,25 @@ export async function GET() {
           price_data: {
             currency: 'USD',
             product_data: {
-              name: "ChatPDF",
-              description: "Unlimited PDF sessions!",
+              name: 'ChatPDF',
+              description: 'Unlimited PDF sessions!',
             },
-            unit_amount: 2000,
+            unit_amount: 200,
             recurring: {
               interval: 'month',
-            }
+            },
           },
           quantity: 1,
-        }
+        },
       ],
       metadata: {
         userId,
-      }
-    })
+      },
+    });
 
-    return NextResponse.json({ url: stripeSession.url })
+    return NextResponse.json({ url: stripeSession.url });
   } catch (error) {
-    console.log('stripe error', error)
+    console.log('stripe error', error);
     return NextResponse.json(error, { status: 500 });
   }
 }
